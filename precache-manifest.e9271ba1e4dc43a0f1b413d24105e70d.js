12521af77d42625cd5bef075572335ddd480dda4
jonisorj <47850849+jonisorj@users.noreply.github.com>
2019-02-21 11:54:46 +0200

    Setup-commit

---
+++ precache-manifest.e9271ba1e4dc43a0f1b413d24105e70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "54fe3c28b05f42ad26f2",
+    "url": "/weather-app/static/js/main.54fe3c28.chunk.js"
+  },
+  {
+    "revision": "8d164941487123aaf291",
+    "url": "/weather-app/static/js/2.8d164941.chunk.js"
+  },
+  {
+    "revision": "54fe3c28b05f42ad26f2",
+    "url": "/weather-app/static/css/main.3471f6bb.chunk.css"
+  },
+  {
+    "revision": "60f7338d546a52e7cdf89b025cdb42d4",
+    "url": "/weather-app/index.html"
+  }
+];
